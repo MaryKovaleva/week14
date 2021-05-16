6b73092b799231b483404de83f4db613b69c8ca6
Maria Kovaleva <77640671+MaryKovaleva@users.noreply.github.com>
2021-05-16 14:54:46 +0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,5 @@
+function ChangeSrc(){
+    document.getElementById("photo1").value=src='olympus2.jpg';
+    }
+    //function setBigImage(foto) {//
+    //document.getElementById("photo2").src = foto.src;}//
