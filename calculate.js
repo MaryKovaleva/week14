35a52c930ca1d2afa279aa60da9ce866d6c70864
Maria Kovaleva <77640671+MaryKovaleva@users.noreply.github.com>
2021-05-22 18:20:32 +0300

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -6,22 +6,22 @@ function Sum(a,b){
     return a+b;
 }
 let result=Sum(document.getElementsById('firstnumber').value,document.getElementById('secondnumber').value);
-console.log(result);
+document.getElementById("1number").value = result;
 
 function Sub(a,b){
     return a-b;
 }
 let result=Sub(document.getElementsById('1number').value,document.getElementById('2number').value);
-console.log(result);
+document.getElementById("3number").value = result;
 
 function Div(a,b){
     return a/b;
 }
 let result=Div(document.getElementsById('3number').value,document.getElementById('4number').value);
-console.log(result);
+document.getElementById("4number").value = result;
 
 function Multi(a,b){
     return a*b;
 }
 let result=Multi(document.getElementsById('Thirdnumber').value,document.getElementById('Fourthnumber').value);
-console.log(result);
+document.getElementById("5number").value = result;
