f783d1f8ab619d5a3e77c34bd4f2826c72e015ba
Maria Kovaleva <77640671+MaryKovaleva@users.noreply.github.com>
2021-05-27 22:22:17 +0300

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -1,27 +1,36 @@
-//let Number1 = prompt('Первое число');
-//let Number2 = prompt ('Второе число');//
-//alert(+Number1 + +Number2);//
+
 
 function Sum(a,b){
     return a+b;
 }
-let result=Sum(document.getElementsById('firstnumber').value,document.getElementById('secondnumber').value);
-document.getElementById("1number").value = result;
+
+function CalcSum() {
+let result=Sum(document.getElementsById('firstnumber').value, document.getElementById('secondnumber').value);
+document.getElementById("Sum").value = result;
+}
 
 function Sub(a,b){
     return a-b;
 }
-let result=Sub(document.getElementsById('1number').value,document.getElementById('2number').value);
-document.getElementById("3number").value = result;
+
+function CalcSub() {
+    let result=Sub(document.getElementsById('1number').value,document.getElementById('2number').value);
+document.getElementById("Sub").value = result;
+}
+
 
 function Div(a,b){
     return a/b;
 }
+
+function CalcDiv() {
 let result=Div(document.getElementsById('3number').value,document.getElementById('4number').value);
-document.getElementById("4number").value = result;
+document.getElementById("Div").value = result;}
 
 function Multi(a,b){
     return a*b;
 }
-let result=Multi(document.getElementsById('Thirdnumber').value,document.getElementById('Fourthnumber').value);
-document.getElementById("5number").value = result;
+
+function ClacMulti() {
+let result=Multi(document.getElementsById('5number').value,document.getElementById('6number').value);
+document.getElementById("5number").value = result;}
