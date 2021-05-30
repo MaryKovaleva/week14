136436529667db4fa8934f160cbf211d7dbb5394
Maria Kovaleva <77640671+MaryKovaleva@users.noreply.github.com>
2021-05-30 17:50:34 +0300

    Add files via upload
    форма входа на сайт

---
+++ week15.js
@@ -0,0 +1,32 @@
+function check()
+{let name = document.getElementById("exampleInputName");
+let email = document.getElementById("exampleInputEmail1");
+let surname = document.getElementById("exampleInputSurname");
+let password = document.getElementById("exampleInputPassword1");
+let password2 = document.getElementById("exampleInputPassword2");
+
+document.getElementById('ErrorMessage')
+.innerHTML = "";
+
+if (name.value == "") {
+document.getElementById('ErrorMessage')
+.innerHTML+= "Ваше имя не заполненно<br>";}
+
+if (email.value == "") {
+    document.getElementById('ErrorMessage')
+    .innerHTML+= "Ваша почта не заполненна<br>";}
+
+if (surname.value == "") {
+    document.getElementById('ErrorMessage')
+    .innerHTML+= "Ваша фамилия не заполненна<br>";}
+
+if (password.value == "") {
+    document.getElementById('ErrorMessage')
+    .innerHTML+= "Ваш пароль не заполнен<br>";}
+
+if (password2.value == ""){
+    document.getElementById('ErrorMessage')
+    .innerHTML+= "Допущена ошибка в повторе пароля";
+}
+
+}
