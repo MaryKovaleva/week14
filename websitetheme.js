228561c3d348861474f6fd3e44dbe29d30f75b6d
Maria Kovaleva <77640671+MaryKovaleva@users.noreply.github.com>
2021-05-30 22:32:55 +0300

    Add files via upload
    сайт меняет цвета

---
+++ websitetheme.js
@@ -0,0 +1,15 @@
+function Change(color) 
+{ switch (document.getElementById("color").value)
+{
+    case "red":document.getElementById('body').style.background='red';break;
+
+    case "blue":document.getElementById('body').style.background='blue';break;
+    
+    case "green":document.getElementById('body').style.background='green';break;
+
+    case "purple":document.getElementById('body').style.background='purple';break;
+}
+
+
+
+}
